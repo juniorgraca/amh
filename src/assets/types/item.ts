c2803919dd4d7e516bb6cc199a963a00a1db3731
junior <wolfnygard@gmail.com>
2024-05-06 21:52:09 -0300

    updated

---
+++ src/assets/types/item.ts
@@ -0,0 +1,55 @@
+export {item};
+
+interface Propsitem {
+id:number,
+name:string,
+mark:string,
+desc:string,
+img:string
+
+}
+const item:Propsitem [] =[
+    {
+        id:0,
+        name:'ONU R1 EPON GPON',
+        mark:'Intelbras',
+        desc:'Onu R1 Epon Gpon Intelbras 1porta Giga Fibra Oti',
+        img:'/logoeqp/intelbras.png'
+    },
+    {
+        id:1,
+        name:'ONU R3',
+        mark:'Intelbras',
+        desc:'Onu R3Oti',
+        img:'/logoeqp/intelbras.png'
+    },
+    {
+        id:2,
+        name:'ONU R3',
+        mark:'Intelbras',
+        desc:'Onu R3Oti',
+        img:'/logoeqp/intelbras.png'
+    },
+    {
+        id:3,
+        name:'ONU R3',
+        mark:'Intelbras',
+        desc:'Onu R3Oti',
+        img:'/logoeqp/intelbras.png'
+    },
+    {
+        id:4,
+        name:'ONU R3',
+        mark:'Intelbras',
+        desc:'Onu R3Oti',
+        img:'/logoeqp/intelbras.png'
+    }
+    , {
+        id:5,
+        name:'ONU R3',
+        mark:'Intelbras',
+        desc:'Onu R3Oti',
+        img:'/logoeqp/intelbras.png'
+    }
+
+]
--- src/pages/IndexPage/Index.tsx
+++ src/pages/IndexPage/Index.tsx
@@ -1,7 +1,9 @@
-import huawei from "./../../../public/logoeqp/huawei.png"
-import itb from "./../../../public/logoeqp/intelbras.png"
-import zte from "./../../../public/logoeqp/zte.png"
-import zx from "./../../../public/logoeqp/zyxel.png"
+import search from "/img/search.svg"
+import itb from "/logoeqp/intelbras.png"
+
+import { item } from "../../assets/types/item"
+
+
 function Index() {
   return (
       <>
@@ -11,11 +13,39 @@ function Index() {
     </h1>
     </div>
     <div>
-    <h2 className="bg-color bg-slate-500 flex text-center justify-around">Segunda parte</h2>
-    <img src={huawei} alt="" />
-    <img src={itb} alt="" />
-    <img src={zte} alt="" />
-    <img src={zx} alt="" />
+    <div className="flex w-full" >
+    <div className="bg-color justify-center px-4 py-4 w-full bg-slate-500 flex text-center"> <img src={search} alt=""
+     width="auto" height="auto" className="max-w-7 max-h-13 w-full" />
+     <input type="text" className=" bg-slate-900 px-5 mx-4 w-1/4"placeholder="Barra de pesquisa" /></div>
+     </div>
+    <section className="flex margin m-8 justify-center ">
+    <div className="flex tex-center justify-center m-8 items-center">
+    <div className="imgFundo">
+    <img src={itb} width="auto" height="auto" alt="img huawei" className="max-w-60 max-h-13 w-full" />
+    </div>
+    <div className="imgFundo">
+    <img src={itb} width="auto" height="auto" alt="img zte" className="max-w-60 max-h-13 w-full"  />
+    </div>
+    <div className="imgFundo">
+    <img src={itb} width="auto" height="auto" alt="img itb" className="max-w-60 max-h-13 w-full"  />
+    </div>
+    <div className="imgFundo">
+    <img src={itb} width="auto" height="auto" alt="img itb" className="max-w-60 max-h-13 w-full"  />
+    </div>
+    </div>
+    </section>
+    <section className="grid grid-cols-3 grid-rows-3 gap-0 w-144">
+    {item.map((item,index)=>(
+      <div className=" grid grid-cols-4 bg-slate-600 w-96 h-80 mx-8 my-8 rounded-lg" key={index}>
+        <div className="">
+        <img className="" src={item.img} alt="" />
+        <p>{item.name}</p>
+        <p>{item.mark}</p>
+        <p>{item.desc}</p>
+        </div>
+      </div>
+    ))}
+    </section>
     </div>
       </>
   )
